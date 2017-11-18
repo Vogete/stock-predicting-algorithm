3178693f6c5fc23127080ab386d3db60f38c28c5
Vogete <fraknoiz@gmail.com>
2017-11-18 18:53:01 +0100

    refactoring, nytimes csv converter modularizing and refactoring

--- data-preprocessing/article-APIs/nytimes_json-to-csv.js
+++ data-preprocessing/article-APIs/nytimes_json-to-csv.js
@@ -1,91 +1,30 @@
-var fs = require('fs');
-var json2csv = require('json2csv');
 var common = require('./common.js');
-
-
-function convertToCSVasync(fileJSON, fileCSV, title = false) {
-    fs.readFile(fileJSON, 'utf8', function (err, data) {
-        if (err) throw err;
-        obj = JSON.parse(data);
-
-        var columns = [
-            "pub_date",
-            "snippet",
-            "headline.main"
-        ];
-
-        try {
-            var csv = json2csv({
-                data: obj,
-                fields: columns,
-                hasCSVColumnTitle: title
-            });
-
-            fs.writeFile(fileCSV, csv, function (err) {
-                if (err) throw err;
-                console.log('file saved');
-            });
-
-        } catch (err) {
-            // Errors are thrown for bad options, or if the data is empty and no fields are provided.
-            // Be sure to provide fields if it is possible that your data array will be empty.
-            console.error(err);
-        }
-
-    });
-
-}
-
-function convertToCSV(jsonData, hasTitle = false) {
-    var columns = [
-        "pub_date",
-        "snippet",
-        "headline.main"
-    ];
-
-    try {
-        var csv = json2csv({
-            data: jsonData,
-            fields: columns,
-            hasCSVColumnTitle: hasTitle
-        });
-
-        return csv;
-    } catch (err) {
-        console.error(err);
-    }
-
-}
-
-function readFromJsonFile(JSON_Path) {
-    var data = fs.readFileSync(JSON_Path, 'UTF-8');
-    var jsonData = JSON.parse(data);
-    return jsonData;
-}
-
-
-function writeToCSVFile(CSV_Path, data) {
-
-    fs.writeFileSync(CSV_Path, data);
-
-}
 
 
 (function main() {
     var beginDate = new Date();
     var endDate = new Date();
-    beginDate.setFullYear(2017, 0, 1);
-    endDate.setFullYear(2017, 10, 13);
-
-    var includeHeader = true;
     var csvData = [];
     var jsonData = [];
+
+
+    beginDate.setFullYear(2017, 0, 1);
+    endDate.setFullYear(2017, 10, 13);
+    var csvOptions = {
+        includeHeader: true,
+        columns: [
+            "pub_date",
+            "snippet",
+            "headline.main"
+        ]
+    };
+
     var currentDay = new Date(beginDate);
 
     while (currentDay < endDate) {
         var currentFileName = common.dateConverter(currentDay) + "-" + common.dateConverter(common.addDay(currentDay)) + "_nytimes";
 
-        var tempjson = readFromJsonFile("./data/" + currentFileName + ".json");
+        var tempjson = common.readFromJsonFile("../../assets/nytimes/json-articles/" + currentFileName + ".json");
         for (let i = 0; i < tempjson.length; i++) {
             jsonData.push(tempjson[i]);
         }
@@ -93,10 +32,8 @@ function writeToCSVFile(CSV_Path, data) {
         currentDay = common.addDay(currentDay);
     }
 
-    // convertToCSVasync("20171110-20171111_nytimes.json", "20171110-20171111_nytimes.csv");
-    csvData = convertToCSV(jsonData, includeHeader);
-
-    writeToCSVFile("trainingData.csv", csvData);
+    csvData = common.convertToCSV(jsonData, csvOptions.columns, csvOptions.includeHeader);
+    common.writeToCSVFile("../../assets/nytimes/nytimes-articles.csv", csvData);
 
 
 })();
