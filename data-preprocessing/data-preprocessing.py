c782e643b82f105f0b656194901848f076aa01d7
Andras Csepreghy <andris788@gmail.com>
2017-11-18 23:46:26 +0100

    Created init function from which all the rest is called

--- data-preprocessing/data-preprocessing.py
+++ data-preprocessing/data-preprocessing.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# stock_filename = 'stock-prices/AAPL-60min.csv'
-stock_filename = 'stock-prices/AAPL-day.csv'
+def create_constants():
+    print 'Creating constants...'
+    constants = {
+        'stock_filename': 'stock-prices/AAPL-day.csv'
+        #'stock_filename': 'stock-prices/AAPL-60min.csv'
+    }
+
+    return constants
 
 def create_dataframe(stock_filename, has_time):
+    print 'Creating dataframe with stock_filename ', stock_filename, 'and has_time ', has_time
     if has_time == True:
         columns = ['date', 'time', 'open', 'high', 'low', 'close', 'volume']
         df = pd.read_csv(stock_filename, names=columns)
@@ -22,14 +29,14 @@ def create_dataframe(stock_filename, has_time):
 
     return df
 
-df = create_dataframe(stock_filename, False)
-
 # Creates a moving average of 8 items (hours or half an hours) so the intraday ups and downs are less drastic
 
-df['close 8ma'] = df['close'].rolling(window=8, min_periods=0).mean()
+def create_moving_average(df):
+    print 'Creating moving averages'
+    df['close 8ma'] = df['close'].rolling(window=8, min_periods=0).mean()
 
-def add_change_column():
-    print len(df)
+def add_change_column(df):
+    print 'Adding change column'
     for i, row in df.iterrows():
         if i < (len(df) - 1):
             close = df.loc[i, 'close 8ma']
@@ -41,15 +48,22 @@ def add_change_column():
             print df.loc[i]
 
     df.dropna(inplace=True)
+    print df
 
-add_change_column()
-
-print df
-
-def plot_stock():
+def plot_stock(df):
+    print 'Creating the plot'
     ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 
     ax1.plot(df.index, df['close'])
     ax1.plot(df.index, df['close 8ma'])
 
     plt.show()
+
+def init():
+    constants = create_constants()
+    df = create_dataframe(constants['stock_filename'], False)
+    create_moving_average(df)
+    add_change_column(df)
+    # plot_stock(df)
+
+init()
