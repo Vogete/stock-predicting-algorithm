8e0a5eafb88851f42bfed4fe29b63ded1315d21a
Vogete <fraknoiz@gmail.com>
2017-11-18 18:53:01 +0100

    refactoring, nytimes csv converter modularizing and refactoring

--- data-preprocessing/article-APIs/common.js
+++ data-preprocessing/article-APIs/common.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
+var json2csv = require('json2csv');
+
+
 Date.prototype.yyyymmdd = function () {
     var mm = this.getMonth() + 1; // getMonth() is zero-based
     var dd = this.getDate();
@@ -23,5 +27,26 @@ module.exports.addDay = function (date, numberOfDays = 1) {
     return subtractedDay;
 };
 
+module.exports.readFromJsonFile = function (JSON_Path, encoding = "UTF8") {
+    var data = fs.readFileSync(JSON_Path, encoding);
+    var jsonData = JSON.parse(data);
+    return jsonData;
+};
 
+module.exports.writeToCSVFile = function (CSV_Path, data) {
+    fs.writeFileSync(CSV_Path, data);
+};
 
+module.exports.convertToCSV = function (jsonData, columns, hasTitle = true) {
+    try {
+        var csv = json2csv({
+            data: jsonData,
+            fields: columns,
+            hasCSVColumnTitle: hasTitle
+        });
+        return csv;
+
+    } catch (err) {
+        console.error(err);
+    }
+};
