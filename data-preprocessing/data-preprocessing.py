966b5ebb67d4f78b94ee272d6d254cf05d78d83b
Andras Csepreghy <andris788@gmail.com>
2017-11-20 14:09:19 +0100

    Converts stock date to UTC

--- data-preprocessing/data-preprocessing.py
+++ data-preprocessing/data-preprocessing.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from datetime import datetime
+import pytz
+from pytz import timezone
 
 
 def create_constants():
@@ -19,7 +21,8 @@ def create_dataframe(stock_filename, has_time):
         columns = ['date', 'time', 'open', 'high', 'low', 'close', 'volume']
         df = pd.read_csv(stock_filename, names=columns)
         df = pd.DataFrame({
-            'datetime': df['date'] + '-' + df['time'],
+            'date': df['date'],
+            'time': df['time'],
             'close': df['close']
         })
 
@@ -46,13 +49,28 @@ def add_change_column(df):
             close = df.loc[i, 'close 8ma']
             close_next = df.loc[i + 1, 'close 8ma']
             close_change = (close_next - close)/close*100
-
             df.loc[i, 'change'] = close_change
-
             print df.loc[i]
 
     df.dropna(inplace=True)
     print df
+
+def change_date_to_utc(df):
+    print 'Changing datetime to UTC'
+    for i, row in df.iterrows():
+        date = df.loc[i, 'date'].split('/')
+        time = df.loc[i , 'time'].split(':')
+
+        year = int(date[2])
+        month = int(date[0])
+        day = int(date[1])
+        hour = int(time[0])
+        minute = int(time[1])
+
+        ny_timezone = timezone('America/New_York')
+        eastern = timezone('US/Eastern')
+        utc = datetime(year, month, day, hour, minute, 0, 0, tzinfo=pytz.FixedOffset(-300))
+        print utc
 
 def plot_stock(df):
     print 'Creating the plot'
@@ -65,9 +83,10 @@ def plot_stock(df):
 
 def init():
     constants = create_constants()
-    df = create_dataframe(constants['stock_filename'], False)
-    create_moving_average(df)
-    add_change_column(df)
+    df = create_dataframe(constants['stock_filename'], True)
+    # create_moving_average(df)
+    # add_change_column(df)
+    change_date_to_utc(df)
     print df.head()
     # plot_stock(df)
 
