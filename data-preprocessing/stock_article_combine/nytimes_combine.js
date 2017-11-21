c7377dd63fd9b6746808a2e07090c42564316248
Vogete <fraknoiz@gmail.com>
2017-11-21 23:18:32 +0100

    data combine stage 1, search function works

--- data-preprocessing/stock_article_combine/nytimes_combine.js
+++ data-preprocessing/stock_article_combine/nytimes_combine.js
@@ -1,6 +1,5 @@
 var common = require("../common.js");
 
-
 // - title
 // - descripton (vagy snippet vagy mi)
 // - pubdate_epoch
@@ -10,11 +9,37 @@ var common = require("../common.js");
 // - close
 // - 8ma_close
 // - change
-var stockPath = "../../assets/stock/AAPL/datetime-utc-close-8ma-change/AAPL-60min.csv";
-var articlePath = "../../assets/news_articles/nytimes/nytimes-articles.csv";
+
+function searchStockForArticle(stocks, article) {
+    var dateHeader = {
+        article: "pub_date_epoch",
+        stock: "utc"
+    };
+
+    var currArticleEpoch = article[dateHeader.article];
+
+    for (let i = 0; i < stocks.length; i++) {
+        var currStockEpoch = common.dateToEpoch( stocks[i][dateHeader.stock] );
+
+        if (parseInt(currStockEpoch) < parseInt(currArticleEpoch)) {
+            // move on baby
+
+        } else if (parseInt(currStockEpoch) >= parseInt(currArticleEpoch)) {
+            console.log("match found");
+            console.log(currArticleEpoch);
+            console.log(currStockEpoch);
+            return;
+        }
+
+    }
+
+
+}
 
 function main() {
 
+    var articlePath = "../../assets/news_articles/nytimes/nytimes-articles.csv";
+    var stockPath = "../../assets/stock/AAPL/datetime-utc-close-8ma-change/AAPL-60min.csv";
     var stockOptions = {
         delimiter: ";"
     };
@@ -23,8 +48,10 @@ function main() {
     var promiseStock = common.readFromCSVFile(stockPath, stockOptions);
 
     Promise.all([promiseArticle, promiseStock]).then(results => {
-        console.log(results[0]);
+        // console.log(results[1]);
+        searchStockForArticle(results[1], results[0][1]);
     });
+
 
 
 
