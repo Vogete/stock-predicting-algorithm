97f47a8bce48d146e2632c4735b92e776c518318
Andras Csepreghy <andris788@gmail.com>
2017-11-27 02:59:05 +0100

    Vocabulary is created, and whole dataframe is one-hot-encoded

--- data-preprocessing/nlp.py
+++ data-preprocessing/nlp.py
@@ -1,15 +1,20 @@
 # import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+import json
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from data_preprocessing import read_csv
 import sys
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.feature_extraction.text import CountVectorizer
 
 stop_words = set(stopwords.words("english"))
 print stop_words
 
 ps = PorterStemmer()
+vectorizer = CountVectorizer()
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -26,52 +31,125 @@ def create_corpus(df):
 
     for i, title in news_titles.iteritems():
         words = word_tokenize(title)
-        filtered_title = []
+        stemmed_title = []
 
         for word in words:
             if word not in stop_words:
-                filtered_title.append(word.decode('utf-8').replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201d", "").lower())
-                print word
+                formatted_word = word.decode('utf-8').replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201d", "").lower()
+                stemmed_word = ps.stem(formatted_word)
 
-        print filtered_title
-        stemmed_title = []
-        for word in filtered_title:
-            if word not in corpus:
-                corpus.append(word)
-            stemmed_title.append(ps.stem(word))
+                stemmed_title.append(stemmed_word)
 
-        print stemmed_title
+                if stemmed_word not in corpus:
+                    corpus.append(stemmed_word)
 
         df.set_value(i, 'title', stemmed_title)
 
-    for i, title in news_description.iteritems():
-        words = word_tokenize(title)
-        filtered_description = []
+    for i, description in news_description.iteritems():
+        words = word_tokenize(description)
+        stemmed_description = []
 
         for word in words:
             if word not in stop_words:
-                filtered_description.append(word.decode('utf-8').replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201d", "").lower())
-                print word
+                formatted_word = format_word(word)
+                stemmed_word = ps.stem(formatted_word)
 
-        print filtered_description
-        stemmed_description = []
-        for word in filtered_description:
-            if word not in corpus:
-                corpus.append(word)
-            stemmed_description.append(ps.stem(word))
+                stemmed_description.append(stemmed_word)
 
-        print stemmed_description
+                if stemmed_word not in corpus:
+                    corpus.append(stemmed_word)
 
         df.set_value(i, 'description', stemmed_description)
 
     return corpus
 
+def read_corpus_from_txt():
+    with open("corpus.txt", "r") as f:
+        read_corpus = json.load(f)
+        print "read_corpus"
+        print read_corpus
+    return read_corpus
+
 def write_corpus_to_txt(corpus):
-    text_file = open("corpus.txt", "w")
-    text_file.write(str(corpus))
-    text_file.close()
+    with open("corpus.txt", "w") as f:
+        return json.dump(corpus, f)
+
+def format_word(word):
+    return word.decode('utf-8')\
+               .replace(u"\u2018", "")\
+               .replace(u"\u2019", "")\
+               .replace(u"\u201c", "")\
+               .replace(u"\u201d", "")\
+               .lower()
 
 corpus = create_corpus(df_stock_news)
+# print corpus
+# write_corpus_to_txt(corpus)
 
-print len(corpus)
+# corpus = read_corpus_from_txt()
+
+# label_encoder = LabelEncoder()
+
+# encoded_corpus = label_encoder.fit_transform(corpus)
+
+# print "encoded_corpus"
+# print encoded_corpus
+
+# corpus = read_corpus_from_txt()
+
+
+
+# print bag_of_words
+
+# print 'bag_of_words'
+# print bag_of_words
+
+bag_of_words = vectorizer.fit_transform(corpus)
+bag_of_words = bag_of_words.toarray()
+vocab = vectorizer.get_feature_names()
+
+print 'appl', vectorizer.vocabulary_.get("basketbal")
+print "vocab getme apple"
+print vocab[vocab.index("basketbal")]
+
+print df_stock_news.columns[3]
+
+zeros = []
+for w in vocab:
+    zeros.append(0)
+
+df_training_data = pd.DataFrame(0, index=np.arange(df_stock_news.shape[0]), columns=vocab)
+# df_training_data.to_csv('zeros.csv')
+
+for i, row in df_stock_news.iterrows():
+    title = df_stock_news.loc[i, 'title']
+    title = title
+    for word in title:
+        # print ps.stem(format_word(word))
+        if word in vocab:
+            print word
+            df_training_data.set_value(i, word, df_training_data.loc[i, word] + 1)
+
+for i, row in df_stock_news.iterrows():
+    description = df_stock_news.loc[i, 'description']
+    description = description
+    for word in description:
+        # print ps.stem(format_word(word))
+        if word in vocab:
+            print word
+            df_training_data.set_value(i, word, df_training_data.loc[i, word] + 1)
+
+print df_training_data
+
+df_training_data.to_csv('training_data.csv')
+
+# For each, print the vocabulary word and the number of times it
+# appears in the training set
+# for tag, count in zip(vocab, dist):
+#     print count, tag
+
+
+# onehotencoder = OneHotEncoder(categorical_features=[0, 1])
+# df_stock_news = onehotencoder.fit_transform(df_stock_news).toarray()
+
 print df_stock_news.head()
